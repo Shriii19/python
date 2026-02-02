0d5384683470b2686386519c76ddf066c220acf5
shriii19 <mudabeshrinivas@gmail.com>
2026-02-02 14:40:47 +0530

    Add examples for string indexing, slicing, negative slicing, and various string functions

---
+++ Day4/Strings.py
@@ -0,0 +1,35 @@
+# Indexing
+
+print("1.Indexing")
+str1="hey horizon"
+print(str1[4]) #we cannot replace the words init
+
+#Slicing
+
+print("2.Slicing")
+str2="hello horizon"
+print(str2[0:3]) #it will give that letter btn [start : end] 
+
+#Navgative Slicing
+
+print("3.Navgative Slicing")
+str3="apple"
+print(str3[-5:-1])
+
+#String Functions
+
+print("4.String Functions")
+str4="hello i m you tuber with 130 sub and name is horizon gaming"
+
+# type of output
+
+print(" 4.1 endswith")
+print(str4.endswith("gaming")) #it will check the the work that is end with correct or not if it is correct then true if false [just change the gaming]
+print(" 4.2 Capitalize")
+print(str4.capitalize()) #it will make the 1st word capital 
+print(" 4.3 Replace")
+print(str4.replace("you" , "U")) #it will replace the words
+print(" 4.4 Find")
+print(str4.find("i")) #it will find the word
+print(" 4.5 Count")
+print(str4.count("s")) #it will count the lettle how many time it come
