179e117fa837dc918888425e5d44cd4b435523b7
shriii19 <mudabeshrinivas@gmail.com>
2026-02-03 15:58:13 +0530

    Add movie input program to collect and display user-entered movie names

---
+++ Day4/1ist.py
@@ -0,0 +1,33 @@
+# movies=[]
+# mov1=input(str("enter 1 name"))
+# mov2=input(str("enter 2 name"))
+# mov3=input(str("enter 3 name"))
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+# print(movies)
+
+
+# -***********Methods 2**********
+
+# movies=[]
+# mov1=input(str("enter 1 name"))
+# movies.append(mov1)
+# mov2=input(str("enter 2 name"))
+# movies.append(mov2)
+# mov3=input(str("enter 3 name"))
+# movies.append(mov3)
+
+# print(movies)
+
+
+# -***********Methods 3**********
+
+movies=[]
+
+mov1=movies.append(str(input("enter ur 1 movies name")))
+mov2=movies.append(str(input("enter ur 2 movies name")))
+mov3=movies.append(str(input("enter ur 3 movies name")))
+
+print(movies)
