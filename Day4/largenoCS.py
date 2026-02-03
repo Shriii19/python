44fbdd2e8293d8bbc3297cd4c1c9ddab0efa8345
shriii19 <mudabeshrinivas@gmail.com>
2026-02-03 15:34:32 +0530

    Add program to determine the largest of four input numbers

---
+++ Day4/largenoCS.py
@@ -0,0 +1,17 @@
+a = int(input("enter the 1 No:"))
+b = int(input("enter the 2 No:"))
+c = int(input("enter the 3 No:"))
+d = int(input("enter the 4 No:"))
+
+
+if a >=b and a >=c and a >=d:
+    print("a is biggest No:", a)
+
+elif b >=c and b >=d:
+        print("b is biggest no:" , b)
+
+elif c >=d:
+      print("c is biggest No:" ,c)
+
+else:
+      print("d is biggest no:", d)
