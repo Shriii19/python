e433a235fb312c4efc0c19c656e948f39d15ca1e
shriii19 <mudabeshrinivas@gmail.com>
2026-02-06 23:27:35 +0530

    Add initial dictionary and methods to display its contents

---
+++ 1dist.py
@@ -0,0 +1,19 @@
+info ={
+    "name" : "jon",
+    "last name" : "baba",
+    "location" : "usa",
+    "age": 22
+
+}
+
+print(info)
+
+# methods
+
+print(info.keys())
+print(info.values())
+print(info.items())
+print(info.get("name"))
+info2 = {"city" : "new york" , "time": 330}
+info.update
+print(info)
