356367937027b263ee442852c0a283873e1291bc
shriii19 <mudabeshrinivas@gmail.com>
2026-02-06 14:51:56 +0530

    Add email validation program to check for '@' and '.com' in user input

---
+++ task.py
@@ -0,0 +1,16 @@
+# email print check me is there or not if there then check @ is there or not
+
+mail = input("enter ur mail: ")
+
+# str ="@gmail.com"
+# str1 = ".com"
+
+if "@" in mail and ".com" in mail:
+    print("u can go")
+
+elif "@" not in mail:
+    print("check ur mail")
+
+else:
+    print("mail is mt")
+ 
