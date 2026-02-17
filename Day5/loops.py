36b38dd2aba94bc151976cda6e095a9d605351b2
shriii19 <mudabeshrinivas@gmail.com>
2026-02-17 22:00:05 +0530

    Add loop implementation to print the multiplication table of 5

---
+++ Day5/loops.py
@@ -0,0 +1,14 @@
+i=1
+while i <= 10:
+    print(5*i)
+    i += 1
+
+
+# i is that we are starting our digit like i=1 we are starting from io1 
+#  while i <= , i >=  , i< or i> this is condition go till that point eg 10:
+
+# then loop go till 10 whether it is <= ,>= , < or >
+
+# print what we are going to print we can pirnt many thing simple i where it wil go till 10 before we put 5*i to print 5 table 
+
+# while is where it will go 
