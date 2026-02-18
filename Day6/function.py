a2b766e74ad62e48eacec7cc3866027c16dc6942
shriii19 <mudabeshrinivas@gmail.com>
2026-02-18 16:19:09 +0530

    Add initial implementation of functions to calculate sum and print message

---
+++ Day6/function.py
@@ -0,0 +1,18 @@
+# line no 3 nd 4 are *functions*
+
+def numbers(a, b, c): #the (a, b, c) this is parameter
+    return a+b+c
+sum = numbers(1, 2, 3) #   sum= number = function call         (1, 2, 3) this is argument
+print(sum)
+
+
+
+# ex2 
+
+def msgs():
+    print("hey how r u ")
+
+msgs()
+
+
+#we can leave parameter is mt also to call the mags
