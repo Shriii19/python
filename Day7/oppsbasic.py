60982be994511c21d75164590bf6c0f54be01922
shriii19 <mudabeshrinivas@gmail.com>
2026-02-23 11:09:59 +0530

    Add implementation of student class with initialization and example instances

---
+++ Day7/oppsbasic.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self, name, marks):
+        self.name =name
+        self.marks =marks
+        print("adding student info")
+
+s1 = student("shree", 9.00)
+print(s1.name, s1.marks)
+
+
+s2 = student ("rohan", 8.95)
+print(s2.name , s2.marks)
