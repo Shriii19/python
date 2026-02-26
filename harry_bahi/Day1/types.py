5b869ce730cb89c22c055591b32aa44ede249d99
shriii19 <mudabeshrinivas@gmail.com>
2026-02-26 09:39:32 +0530

    Add initial implementation for type conversion from string to float

---
+++ harry_bahi/Day1/types.py
@@ -0,0 +1,8 @@
+
+# in this file we have done convert from str to float like this we can convert from int to str to float and so onn
+
+a = "33.22"
+b = float (a) #a but the type should be float 
+t = type(a)
+
+print(t)
